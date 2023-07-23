6b642ec9c1be7d2ff9c85d40689f3530c922ef6b
Aleksandra-Jab <jab.aleksandra@gmail.com>
2023-07-23 18:10:12 +0200

    added AnimeBrowser

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from "./Header/Header";
+import AnimeBrowser from "./Browser/AnimeBrowser";
 
 const App = () => {
 
     return (
         <div className='app'>
             <Header/>
+            <AnimeBrowser/>
         </div>
     );
 }
