02cb4707a800a625cdc60ff8ed74ea439e97ce9d
Aleksandra-Jab <jab.aleksandra@gmail.com>
2023-07-23 12:17:39 +0200

    created App component returning Header

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header/Header";
+
+const App = () => {
+
+    return (
+        <div className="app">
+            <Header/>
+        </div>
+    );
+}
+
+export default App;
